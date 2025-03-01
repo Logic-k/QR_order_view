5ba9dfc29a9e60bed8b210054e1709c8d0b56213
BOOK-GCIJ1CH854\kimmi <usungmagic123@gmail.com>
2025-03-02 08:42:35 +0900

    시간변경

--- app.py
+++ app.py
@@ -266,7 +266,7 @@ def admin():
     <script>
             setInterval(() => {
                 location.reload();
-            }, 5000); // 5초마다 새로고침
+            }, 15000); // 5초마다 새로고침
         </script>
     <script>
     let refreshTime = 5; // 새로고침 간격 (초 단위)
